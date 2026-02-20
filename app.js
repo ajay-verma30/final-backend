1a5caa43fe789672e8dd9c2d8032caa022958776
Ajay Verma <aryanav30@gmail.com>
2026-02-21 01:35:48 +0530

    Updated system key 2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-require('dotenv').config();
+require('dotenv').config({ path: require('path').resolve(__dirname, './.env') });
 
 const authRoutes = require('./modules/auth/auth.routes');
 const systemRoutes = require('./modules/system/system.routes');
