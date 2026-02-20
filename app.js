fa7e650b01ffe5560f0cf8f32f0fa53cdffd9f5c
Ajay Verma <aryanav30@gmail.com>
2026-02-21 00:43:12 +0530

    Updated Cart contollers

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser()); // ← add this
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:3000"],
+  origin: ["http://localhost:5173", "http://localhost:3000","https://final-backend-1-2vg5.onrender.com"],
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization"],
